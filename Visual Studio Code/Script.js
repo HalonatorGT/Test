8661841983fec02d70edb99edef815dd5220d779
Milan <milanheijboer@outlook.com>
2021-09-20 12:55:57 +0200

    JavaScript bestanden

---
+++ Visual Studio Code/Script.js
@@ -0,0 +1,17 @@
+//dit is commentaar, wordt niet zichtbaar//
+
+/*dit is commentaar over meerdere regels,
+ wordt ook niet zichtbaar*/
+
+let variableA= 'dit is tekst';
+//let is aanpasbaar//
+
+console.log ('tekst');
+document.getElementById ('content');
+const aritcle = document.getElementById ('content');
+//const staat vast, kun je 1 keer gebruiken//
+
+const menuItems =document.getElementById ('menuItems');
+console.log (aritcle);
+console.log (menuItems);
+
