f8e8190cc0cf1f781803d2a6927ddb5216523c51
Milan <milanheijboer@outlook.com>
2021-10-25 09:33:18 +0200

    no message

---
+++ For_Each.js
@@ -0,0 +1,15 @@
+const listitems = document.querySelectorAll('list-item');
+
+listitems.forEach(function(element, index) {
+  element.innerHTML = 'Hello World ' + (index + 1);
+});
+
+
+
+
+for (let i = 0; i < 5; i++) {
+  console.log('Hello ' + i);
+}
+
+
+// for each list instructions
